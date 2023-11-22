d9f7fb74dff195af333f4c38a5075846a78772a3
Orbital Oyster <orbitaloyster@gmail.com>
2023-11-22 10:42:11 +0300

    some adjustments

--- st/config.h
+++ st/config.h
@@ -128,10 +128,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 7;
+static unsigned int defaultrcs = 8;
 
 /*
  * Default shape of cursor

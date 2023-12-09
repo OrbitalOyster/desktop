d49c1f1829e6623d2c6210ccc51e863916b3889e
OrbitalOyster <orbitaloyster@gmail.com>
2023-12-09 18:23:29 +0300

    padding fix

--- dwm/config.h
+++ dwm/config.h
@@ -11,6 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 8;       /* vertical padding of bar */
+static const int sidepad            = 8;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Agave Nerd Font Mono:size=18" };
 static const char dmenufont[]       = "Agave Nerd Font Mono:size=18";
 static const char col_gray1[]       = "#3b4252";
